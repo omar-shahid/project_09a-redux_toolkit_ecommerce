19e1440e728302e1b93e7458094f2949195d34c0
Omar Shahid <omarshahid160@gmail.com>
2021-01-05 12:14:38 +0500

    Removed Boilerplate slice example

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import cartSlice from '../features/cartSlice';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     cart: cartSlice
   },
 });
